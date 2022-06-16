406d418a79c690e05fd61419414eaaba3d3c1465
xrhrepus <xrhrepus@gmail.com>
2022-06-15 23:02:39 -0700

    [Editor] change RenderMaterial to be non abstract

--- HUGE/Framework/Graphics/Inc/RenderMaterial.h
+++ HUGE/Framework/Graphics/Inc/RenderMaterial.h
@@ -1,22 +1,24 @@
 #pragma once
-#include <ShaderEffect.h>
-#include <ShaderEffectBufferType.h>
+#include "ShaderEffect.h"
+#include "ShaderEffectBufferType.h"
 
 namespace H::Graphics
 {
 	class RenderMaterial
 	{
 	public:
-		virtual void Init() = 0;
+		virtual void Init();
 		void Bind() const;
 		void UnBind() const;
 		// set commonly used global resource
 		virtual void SetTransform(const TransformData& data);
 		virtual void SetCamera(const Camera& cam);
 		virtual void SetLight(const DirectionalLight& light);
-		virtual void DebugUI() = 0;
+		virtual void DebugUI();
 		void SetShaderContextDebugUIFunc(std::function<void()> func);
-	protected:
+	private:
+		// using this name to get effect from ShaderEffectManager
+		std::string mShaderEffectName = "Standard";
 		// ref to instance in ShaderEffectManager
 		const ShaderEffect* mShaderEffect;
 		// ShaderEffect creates context. data stored in Material
--- HUGE/Framework/Graphics/Src/RenderMaterial.cpp
+++ HUGE/Framework/Graphics/Src/RenderMaterial.cpp
@@ -1,17 +1,28 @@
 #include "Precompiled.h"
 #include <RenderMaterial.h>
+#include <ShaderEffectManager.h>
 
 void H::Graphics::RenderMaterial::SetShaderContextDebugUIFunc(std::function<void()> func)
 {
 	mDebugUICallBack = std::move(func);
 }
 
+void H::Graphics::RenderMaterial::Init()
+{
+	mShaderEffect = (&ShaderEffectManager::Get()->GetEffect(mShaderEffectName));
+	mShaderEffectContext = mShaderEffect->CreateShaderEffectContext();
+	SetShaderContextDebugUIFunc(mShaderEffectContext->CreateDebugUI());
+}
+
 void H::Graphics::RenderMaterial::Bind() const
 {
+	mShaderEffect->SetContextInfo(*mShaderEffectContext);
+	mShaderEffect->Bind();
 }
 
 void H::Graphics::RenderMaterial::UnBind() const
 {
+	mShaderEffect->UnBind();
 }
 
 void H::Graphics::RenderMaterial::SetTransform(const TransformData& data)
@@ -24,4 +35,16 @@ void H::Graphics::RenderMaterial::SetCamera(const Camera& cam)
 
 void H::Graphics::RenderMaterial::SetLight(const DirectionalLight& light)
 {
+}
+
+void H::Graphics::RenderMaterial::DebugUI()
+{
+	const char* name = mShaderEffectName.c_str();
+	ImGui::PushID(name);
+	if (ImGui::TreeNode(name))
+	{
+		mDebugUICallBack();
+		ImGui::TreePop();
+	}
+	ImGui::PopID();
 }
