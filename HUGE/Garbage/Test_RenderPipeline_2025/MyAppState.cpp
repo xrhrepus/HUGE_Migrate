64e6825c1a924cfeb617070d5bbdaafc1661a735
xrhrepus <xrhrepus@gmail.com>
2025-02-24 20:37:06 -0800

    Added proj for render pipeline prot

---
+++ HUGE/Garbage/Test_RenderPipeline_2025/MyAppState.cpp
@@ -0,0 +1,74 @@
+#include "MyAppState.h"
+#include "HUGE/Inc/TransformComponent.h"
+
+float fps;
+using namespace H::Core;
+using namespace H::Graphics;
+using namespace H::Math;
+using namespace H::Input;
+ 
+void MyAppState::Initialize()
+{
+	mCurrentCam = &mCamera;
+	//camera
+	mCamera.SetPosition({ -121.0f, 75.0f, 100.0f });
+	//mCurrentCam.SetDirection({ 0.0f,0.0f, 1.0f });
+	mCamera.SetDirection(H::Math::Normalize(Vector3{ 121.0f, -75.0f, -100.0f }));
+
+}
+
+void MyAppState::Terminate()
+{
+}
+
+
+void MyAppState::Render()
+{
+	RenderScene();
+}
+
+void MyAppState::Update(float deltatime)
+{
+	mCurrentCam->UpdateControl(deltatime);
+	fps = static_cast<float>(1.0f / deltatime);
+  }
+
+void MyAppState::DebugUI()
+{
+	ImGui::ShowDemoWindow();
+
+	ImGui::Begin("Cam");
+	mCurrentCam->DebugUI();
+	ImGui::End();
+
+ 	ImGui::Begin("Setting");
+	ImGui::Text("fps: %f", fps);
+ 	ImGui::End();
+}
+
+void MyAppState::RenderScene()
+{
+
+
+  	//
+	for (int i = 0; i < 10; i++)
+	{
+		SimpleDraw::AddLine({ -100.0f,0.0f, 10.0f * i }, { 100.0f,0.0f,10.0f * i }, Colors::DarkCyan);
+		SimpleDraw::AddLine({ -100.0f,0.0f , -10.0f * i }, { 100.0f,0.0f,-10.0f * i }, Colors::DarkCyan);
+
+		SimpleDraw::AddLine({ 10.0f * i,0.0f, -100.0f }, { 10.0f * i,0.0f, 100.0f }, Colors::DarkCyan);
+		SimpleDraw::AddLine({ -10.0f * i,0.0f,-100.0f }, { -10.0f * i,0.0f,100.0f }, Colors::DarkCyan);
+
+	}
+
+	SimpleDraw::AddLine({ 0.0f }, { 1.0f,0.0f,0.0f }, Colors::Magenta);//x
+	SimpleDraw::AddLine({ 0.0f }, { 0.0f,1.0f,0.0f }, Colors::LawnGreen);//y
+	SimpleDraw::AddLine({ 0.0f }, { 0.0f,0.0f,1.0f }, Colors::Aqua);//z
+
+	SimpleDraw::Render(*mCurrentCam);
+ 
+}
+
+void MyAppState::PostProcess()
+{
+}
